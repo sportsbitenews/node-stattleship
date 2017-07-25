e89a5bc1296caf8041c2f3e9d5865ce39e799016
David Thyresson <david@stattleship.com>
2017-07-25 12:03:53 -0400

    Update games.js

--- examples/games.js
+++ examples/games.js
@@ -1,6 +1,6 @@
 var StattleshipAPI = require('../index');
 
-var stattleship = new StattleshipAPI('0d742c11259a1a7f1038c0a5506a948e');
+var stattleship = new StattleshipAPI('');
 
 var params = {
   status: 'ended',
